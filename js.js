ccde015807181d8eb8fa1f59b5fadfe1d19e6c43
yasin2210 <130591014+yasin2210@users.noreply.github.com>
2023-04-25 12:33:38 +0330

    Delete js.js

--- js.js
+++
@@ -1,8 +0,0 @@
-var names = ["joe" , "alex" , "Jack", "emmanuel" , "philip"]
-for(var i =0 ; i < 6 ; i++) {
-    if (names[i][0] === "j" || names[i][0] === "J") {
-        console.log("goodbye  " + names[i]);
-    }else{
-        console.log("hello  " + names[i]);
-    }
-}
